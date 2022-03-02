e3857629c16a024e45f5124549a5907aceb706ad
bhagya2002 <bhagya02.p@gmail.com>
2022-03-02 00:51:35 -0700

    new pages added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/HomePage/Home";
+import About from "./components/pages/AboutUs/About";
+import Involve from "./components/pages/GetInvolved/Involve";
 import Footer from "./components/pages/Footer/Footer";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/about-us" exact element={<About />} />
+        <Route path="/get-involved" exact element={<Involve />} />
       </Routes>
       <Footer />
     </Router>
