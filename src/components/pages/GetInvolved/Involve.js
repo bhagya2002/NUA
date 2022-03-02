e042a9407c80a5057f38b44fd075f308958471d5
bhagya2002 <bhagya02.p@gmail.com>
2022-03-02 14:28:17 -0700

    clean up

--- src/components/pages/GetInvolved/Involve.js
+++ src/components/pages/GetInvolved/Involve.js
@@ -30,84 +30,69 @@ function Involve() {
                 by their peers to volunteer their time and effort to represent
                 the Faculty of Nursing's undergraduate students and ensure that
                 they have the best experience possible.
-                <br />
-                <br />
-                The NUA provides academic advocacy, coordinates social events,
-                addresses issues and concerns that affect nursing students, and
-                connects students to the campus community, as well as the
-                national body of nursing students through the Canadian Nursing
-                Students' Association (CNSA).
-                <br />
-                <br />
-                The NUA acts to fulfill the academic, social, and political
-                needs of nursing students. We are the official voice and
-                representation of nursing students on the University of Alberta
-                campuses through the Students' Union, General Faculties Council,
-                Faculty of Nursing, and the Council of Faculty Associations.
-                {/* Role of the NUA */}
-                <br />
-                <br />
-                <strong>Role of the NUA:</strong> <br />
-                <ul>
-                  <li>
-                    Connect students with faculty members through informal and
-                    formal venues and communications
-                  </li>
-                  <li>Organize social and sporting events</li>
-                  <li>Represents students on various University committees</li>
-                  <li>
-                    Offers volunteer activities and opportunities including
-                    local and international levels of involvement
-                  </li>
-                  <li>
-                    Collects and addresses student concerns, and reports them
-                    via the Student Advisory Council
-                  </li>
-                  <li>
-                    Offers involvement in national level organizations,
-                    including the Canadian Nursing Students' Association
-                  </li>
-                  <li>Supports students with online resources</li>
-                  <li>
-                    Liaisons with partnered organizations including Second
-                    Chance CPR and grad photographers
-                  </li>
-                </ul>
-                {/* Our Mission */}
-                <br />
-                <br />
-                <strong>Our Mission:</strong> <br />
-                To create social and academic opportunities through student
-                involvement with the Faculty of Nursing and the University of
-                Alberta. Our Vision:
-                <ul>
-                  <li>
-                    To provide unity among all undergraduate students in the
-                    Faculty of Nursing
-                  </li>
-                  <li>
-                    To promote interest and participation on campus, in the
-                    Faculty of Nursing, and in professional activities
-                  </li>
-                  <li>
-                    To provide an official channel for student-faculty
-                    communication
-                  </li>
-                </ul>
-                {/* Regarding Academics */}
-                <br />
-                <strong>Regarding Academics:</strong>
-                <br /> Academic concerns and interests can be brought forward to
-                your <strong>VP Academic</strong>,
-                <strong> Brett Gleeson </strong>
-                at
-                <strong> gleeson1@ualberta.ca</strong>. Whether you would like
-                to recognize a successful change to your program, are facing a
-                student appeal, or just do not know where to find the form for
-                that exam you missed, we will ensure that your inquiry is dealt
-                with properly or provide you with the appropriate contact
-                information.
               </p>
+              <ul class="cards">
+                <li>
+                  <a href="" class="card">
+                    <img
+                      src="https://static.wixstatic.com/media/14a9c0_b7c33fb81dd0467195e0fa30102ae0e2~mv2.jpg/v1/fill/w_540,h_615,al_c,q_80/14a9c0_b7c33fb81dd0467195e0fa30102ae0e2~mv2.jpg"
+                      class="card__image"
+                      alt=""
+                    />
+                    <div class="card__overlay">
+                      <div class="card__header">
+                        <svg
+                          class="card__arc"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path />
+                        </svg>
+                        {/* <img
+                          class="card__thumb"
+                          src="https://i.imgur.com/7D7I6dI.png"
+                          alt=""
+                        /> */}
+                        <div class="card__header-text">
+                          <h3 class="card__title">Volunteer</h3>
+                        </div>
+                      </div>
+                      <p class="card__description">
+                        Help make Faculty of Nursing events possible!
+                      </p>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="" class="card">
+                    <img
+                      src="https://static.wixstatic.com/media/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg/v1/fill/w_920,h_1048,al_c,q_85,usm_0.66_1.00_0.01/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg"
+                      class="card__image"
+                      alt=""
+                    />
+                    <div class="card__overlay">
+                      <div class="card__header">
+                        <svg
+                          class="card__arc"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path />
+                        </svg>
+                        {/* <img
+                          class="card__thumb"
+                          src="https://i.imgur.com/sjLMNDM.png"
+                          alt=""
+                        /> */}
+                        <div class="card__header-text">
+                          <h3 class="card__title">Meetings</h3>
+                        </div>
+                      </div>
+                      <p class="card__description">
+                        Stay up to date with what's discussed at NUA meetings!
+                      </p>
+                    </div>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
