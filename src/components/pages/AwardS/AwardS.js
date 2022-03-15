d3e8cc95d0a1e7daca9ae701845e3d0a81f33eea
bhagya2002 <bhagya02.p@gmail.com>
2022-03-15 16:25:06 -0600

    awards page completed

--- src/components/pages/AwardS/AwardS.js
+++ src/components/pages/AwardS/AwardS.js
@@ -31,33 +31,15 @@ function AwardS() {
                 the Faculty of Nursing's undergraduate students and ensure that
                 they have the best experience possible.
                 <br />
+                <br />
               </p>
-              <ul class="cards">
-                <li>
-                  <a href="#" class="card">
-                    <img
-                      src="https://static.wixstatic.com/media/14a9c0_b7c33fb81dd0467195e0fa30102ae0e2~mv2.jpg/v1/fill/w_540,h_615,al_c,q_80/14a9c0_b7c33fb81dd0467195e0fa30102ae0e2~mv2.jpg"
-                      class="card__image"
-                      alt=""
-                    />
-                    <div class="card__overlay">
-                      <div class="card__header">
-                        <svg
-                          class="card__arc"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path />
-                        </svg>
-                        {/* <img
-                          class="card__thumb"
-                          src="https://i.imgur.com/7D7I6dI.png"
-                          alt=""
-                        /> */}
-                        <div class="card__header-text">
-                          <h3 class="card__title">Student Choice Awards</h3>
-                        </div>
-                      </div>
-                      <p class="card__description">
+              <div class="container">
+                <div class="column-center">
+                  <article class="card card__content">
+                    <figure class="header"></figure>
+                    <main class="card__body">
+                      <h1 class="title--primary">Student Choice Awards</h1>
+                      <p class="description">
                         Each year students get an opportunity to submit their
                         picks for outstanding clinical, lab, seminar and
                         foundational course instructors. For their dedication,
@@ -66,81 +48,46 @@ function AwardS() {
                         <br />
                         Stay-tuned for communication each winter term!
                       </p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="" class="card">
-                    <img
-                      src="https://static.wixstatic.com/media/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg/v1/fill/w_920,h_1048,al_c,q_85,usm_0.66_1.00_0.01/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg"
-                      class="card__image"
-                      alt=""
-                    />
-                    <div class="card__overlay">
-                      <div class="card__header">
-                        <svg
-                          class="card__arc"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path />
-                        </svg>
-                        {/* <img
-                          class="card__thumb"
-                          src="https://i.imgur.com/sjLMNDM.png"
-                          alt=""
-                        /> */}
-                        <div class="card__header-text">
-                          <h3 class="card__title">
-                            Rachael Longridge Memorial Scholarship
-                          </h3>
-                        </div>
-                      </div>
-                      <p class="card__description">
+                    </main>
+                    <footer class="footer"></footer>
+                  </article>
+                </div>
+                <div class="column-left">
+                  <article class="card card__content">
+                    <figure class="header"></figure>
+                    <main class="card__body">
+                      <h1 class="title--primary">
+                        Rachael Longridge Memorial Scholarship
+                      </h1>
+                      <p class="description">
                         A scholarship created for students emulating Rachael's
                         passion and commitment to nursing as to foster and
                         reward committed, empathetic and motivated nursing
                         students to achieve their goals; and make a difference
-                        just as Rachael did in many of her patient’s lives
+                        just as Rachael did in many of her patient’s lives.
                       </p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="" class="card">
-                    <img
-                      src="https://static.wixstatic.com/media/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg/v1/fill/w_920,h_1048,al_c,q_85,usm_0.66_1.00_0.01/14a9c0_eed97c211f754a0d8862710b65e6d777~mv2_d_3024_4032_s_4_2.jpg"
-                      class="card__image"
-                      alt=""
-                    />
-                    <div class="card__overlay">
-                      <div class="card__header">
-                        <svg
-                          class="card__arc"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path />
-                        </svg>
-                        {/* <img
-                          class="card__thumb"
-                          src="https://i.imgur.com/sjLMNDM.png"
-                          alt=""
-                        /> */}
-                        <div class="card__header-text">
-                          <h3 class="card__title">
-                            NUA Leadership Scholarship
-                          </h3>
-                        </div>
-                      </div>
-                      <p class="card__description">
+                    </main>
+                    <footer class="footer"></footer>
+                  </article>
+                </div>
+                <div class="column-right">
+                  <article class="card card__content">
+                    <figure class="header"></figure>
+                    <main class="card__body">
+                      <h1 class="title--primary">NUA Leadership Scholarship</h1>
+                      <p class="description">
                         The NUA Leadership Scholarship is aimed towards nursing
                         students who are active and instrumental on campus and
                         in the greater Edmonton community regarding nursing and
                         leadership.
                       </p>
-                    </div>
-                  </a>
-                </li>
-              </ul>
+                    </main>
+                    <footer class="footer"></footer>
+                  </article>
+                </div>
+              </div>
+              <br />
+              <br />
             </div>
           </div>
         </div>
