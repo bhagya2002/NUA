08fbf6605ff37bf8681a6a7f8ac6c688899decbe
bhagya2002 <bhagya02.p@gmail.com>
2022-03-16 21:21:36 -0600

    CNSA page updated for info

--- src/components/pages/CNSA/CNSA.js
+++ src/components/pages/CNSA/CNSA.js
@@ -128,6 +128,160 @@ function CNSA() {
                   </li>
                 </ol>
               </p>
+              <br />
+              <br />
+              <h2>
+                <strong>Previous Events</strong>
+              </h2>
+              <h3>
+                <strong>CNSA National Conference</strong>
+              </h3>
+              <p
+                className={{
+                  lightTextDesc: "home__hero-subtitle dark",
+                }}
+                style={{ fontSize: "18px" }}
+              >
+                The CNSA national conference took place in Calgary from January
+                22-26, 2019. The theme for the conference was “Students as
+                Leaders and Advocates”.
+              </p>
+              <br />
+              <br />
+              <h3>
+                <strong>CNSA Regional Conference</strong>
+              </h3>
+              <p
+                className={{
+                  lightTextDesc: "home__hero-subtitle dark",
+                }}
+                style={{ fontSize: "18px" }}
+              >
+                The CNSA hosted the regional conference in Regina and
+                Saskatchewan. Conference dates included October 26-27. This
+                year’s theme was “Breaking Barriers: Exploring the Future
+                Through Rural and Community Nursing."
+                <br />
+                <br />
+                <strong>Notes from the Regional Conference:</strong>
+                <br />
+                At the conference, we voted on several topics and discussed
+                important updates that may be coming to nursing students across
+                Canada. This included discussing the termination of the NCLEX in
+                Newfoundland, the resultant development of a new licensing exam
+                for Canadians (not coming into effect for a few years at least),
+                improvement of evaluations for our nursing practices, advocating
+                for more undergraduate nurse employee (UNE) positions, bilingual
+                programming work, and much more.
+                <br />
+                <br />
+                I was able to attend sessions focused on cannabis use, the
+                opioid crisis, stigmatization of the word "fat", structural
+                violence in Nunvavut, proper patient care for queer individuals,
+                and had the chance to attend a panel session with some of the
+                first responders of the Humboldt Broncos bus tragedy.
+                <br />
+                <br />
+                Lastly, I want to remind you all of some important resources
+                that you receive as a member of a CNSA chapter school!
+                <br />
+                <ul className="list-padding">
+                  <li>
+                    Lippincott NCLEX-RN PassPoint - 25% off their prep guide
+                  </li>
+                  <li>Scholarships available from our stakeholders</li>
+                  <li>Represents students on various University committees</li>
+                  <li>
+                    NurseONE - provides access to best-practice guidelines,
+                    clinical competencies, Anatomy TV, and other educational
+                    resources
+                  </li>
+                  <li>
+                    Longwoods - peer-reviewed leadership journal, contains job
+                    posting boards, and other nursing resources
+                  </li>
+                  <li>
+                    The Personal - decreased group insurance rates (up until 5
+                    years after you graduate)
+                  </li>
+                  <li>
+                    Scrubs with a cause - $0.25 from each scrub set goes towards
+                    funding nursing leadership scholarships
+                  </li>
+                </ul>
+              </p>
+              <br />
+              <br />
+              <h3>
+                <strong>More About Us</strong>
+              </h3>
+              <div class="row">
+                <div class="column">
+                  <h3>Some image here</h3>
+                  <h4>
+                    <strong>CNSA Official Delegate - Rita Iradukunda</strong>
+                  </h4>
+                  <p
+                    className={{
+                      lightTextDesc: "home__hero-subtitle dark",
+                    }}
+                    style={{ fontSize: "1rem" }}
+                  >
+                    CNSA Official Delegate - Rita Iradukunda As Official
+                    Delegate I am the primary resource for nursing students for
+                    CNSA within your chapter school, which is the U of A. My
+                    role is to promote CNSA and all of its associated events. At
+                    regional and national conferences, it is my role to vote on
+                    behalf of you all as students, which is why it is so
+                    important to hear all of your opinions on any national
+                    nursing concerns (such as the NCLEX, etc.). Please feel free
+                    to contact me at riraduku@ualberta.ca if you have any
+                    questions about the CNSA, how you can get involved, or have
+                    a topic or concern that you would like me to bring up with
+                    the CNSA.
+                  </p>
+                </div>
+                <div class="column">
+                  <h3>Some image here</h3>
+                  <h4>
+                    <strong>CNSA Associate Delegate - Jamie Hudson</strong>
+                  </h4>
+                  <p
+                    className={{
+                      lightTextDesc: "home__hero-subtitle dark",
+                    }}
+                    style={{ fontSize: "1rem" }}
+                  >
+                    As the Associate Delegate, I have a very similar role to
+                    that of the official delegate, in that I act as a resource
+                    for nursing students in regards to the CNSA, and step in as
+                    the official delegate (OD) when the OD is unavailable. You
+                    can reach out to me if you have any concerns that you would
+                    like me to address or advocate for related to your
+                    undergraduate nursing education, or for Canadian nursing
+                    students as a whole. I actively promote the CNSA at the
+                    University of Alberta, and try to share with you the
+                    interesting and useful resources that the CNSA has to offer
+                    including discounts, scholarships, and opportunities. At
+                    regional and national conferences, I am responsible to
+                    advocate and/or vote on your behalf, which is why I seek out
+                    your feedback prior to attending these conferences (Ex.
+                    concerns, addressing controversial issues, the NCLEX, etc.).
+                    Basically, the OD and I are your voices at a national level,
+                    and I am happy to provide information and guidance whenever
+                    possible. I am also responsible for planning and executing
+                    the National Nursing Students' Week (NNSW) with the OD,
+                    which occurs annually in November. Please feel free to
+                    contact me at alberta@cnsa.ca or jhudson@ualberta.ca if you
+                    have any questions about the CNSA/NNSW, how you can get
+                    involved at the chapter level, or if you have a topic or
+                    concern that you would like me to address with the CNSA.
+                    Remember: You are important, and deserve the best nursing
+                    school experience possible - speak up and get involved to
+                    see change happen!
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
